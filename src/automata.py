221566b14f30bf976177d901169e2bde1f9c7408
Douglas Weide <douglas.201920299@unilasalle.edu.br>
2024-07-10 02:48:55 -0300

    alteracao do split pra list

--- src/automata.py
+++ src/automata.py
@@ -105,7 +105,7 @@ def process(automata, words):
     Os resultados válidos são ACEITA, REJEITA, INVALIDA.
     """
 
-    words = words.split()
+    words = list(words)
 
     estadoAtual = estadoInicial[0]
 
